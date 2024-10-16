fa6bc5aef21fe82ac7b84495beabaafc95d51e25
Nikita <niknakels@gmail.com>
2024-10-16 15:16:37 +1100

    Add files via upload

---
+++ module_14_1.py
@@ -0,0 +1,70 @@
+import sqlite3
+
+con = sqlite3.connect('not_telegram.db')
+curs = con.cursor()
+
+curs.execute(
+    '''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    balance INTEGER NOT NULL
+    )
+    '''
+)
+con.commit()
+
+for i in range(1, 11):
+    a = i * 10
+    curs.execute(
+        '''
+        INSERT INTO Users (username, email, age, balance)
+        VALUES (?, ?, ?, ?)
+        ''',
+        (f'User{i}', f'example{i}@gmail.com', f'{a}', f'{1000}',)
+    )
+    con.commit()
+
+
+curs.execute(
+    '''
+    UPDATE Users SET balance = 500 WHERE id % 2 != 0
+    '''
+)
+con.commit()
+
+l = []
+c = 0
+for i in range(1, 11):
+    if c == 0:
+        l.append(i)
+    c += 1
+    if c == 3:
+        c = 0
+        continue
+    for j in l:
+        curs.execute(
+            '''
+            DELETE FROM Users
+            WHERE id = ?
+            ''',
+            (j,)
+        )
+        con.commit()
+
+curs.execute(
+    '''
+    SELECT * FROM Users
+    WHERE age <> ?
+    ''',
+    (60,)
+)
+sel = curs.fetchall()
+con.commit()
+
+for i in sel:
+    print(f'Имя: {i[1]}| Почта: {i[2]}| Возраст: {i[3]}| Баланс: {i[4]}')
+
+con.close()
